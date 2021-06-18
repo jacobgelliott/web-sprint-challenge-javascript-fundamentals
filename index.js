1b95c51ea15142f30f42241d285269fa88d4bbc7
JacobGriffithElliott <j.o.gelliott@gmail.com>
2021-06-18 08:46:04 -0700

    MVP in JS, now to complete README

--- index.js
+++ index.js
@@ -197,9 +197,9 @@ CuboidMaker.prototype.surfaceArea = function(){
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
 const cuboid = new CuboidMaker ({
-  length: 10,
-  width: 10,
-  height: 10
+  length: 4,
+  width: 5,
+  height: 5
 });
 
 
@@ -212,14 +212,27 @@ console.log(cuboid.surfaceArea()); // 130
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
-class CuboidMakerTwo{
-
+class CuboidMakerTwo extends CuboidMaker{
+  constructor(attr){
+    super(attr)
+  }
+  volume(){
+    return this.length*this.width*this.height;
+  }
+  surfaceArea(){
+    return 2*(this.length*this.width+this.length*this.height+this.width*this.height);
+  }
 }
 
+const cuboidTwo = new CuboidMaker ({
+  length: 10,
+  width: 10,
+  height: 10
+});
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
